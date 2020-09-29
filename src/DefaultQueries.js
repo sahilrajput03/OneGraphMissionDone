e34f789618e1a1c7d424eb8240f44ddd64779980
Sahil <sahilrajput03@gmail.com>
2020-09-29 20:12:31 +0530

    Initial commit

---
+++ src/DefaultQueries.js
@@ -0,0 +1,45 @@
+export const DEFAULT_QUERY = `
+query MyQuery {
+  hello
+}
+`;
+// export const DEFAULT_QUERY = `# shift-option/alt-click on a query below to jump to it in the explorer
+// # option/alt-click on a field in the explorer to select all subfields
+// query npmPackage {
+//   npm {
+//     package(name: "onegraph-apollo-client") {
+//       name
+//       homepage
+//       downloads {
+//         lastMonth {
+//           count
+//         }
+//       }
+//     }
+//   }
+// }
+
+// query graphQLPackage {
+//   npm {
+//     package(name: "graphql") {
+//       name
+//       homepage
+//       downloads {
+//         lastMonth {
+//           count
+//         }
+//       }
+//     }
+//   }
+// }
+
+// fragment bundlephobiaInfo on BundlephobiaDependencyInfo {
+//   name
+//   size
+//   version
+//   history {
+//     dependencyCount
+//     size
+//     gzip
+//   }
+// }`;
